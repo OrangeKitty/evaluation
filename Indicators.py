1868e46eb45cd6a6423f2bd0ac160b9ab9fe88d1
OrangeKitty <igettogo007@gmail.com>
2018-02-09 00:41:57 +0800

    Drafts
    1st draft.

---
+++ Indicators.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import numpy.ma as ma
+
+
+class indicators(object):
+    
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def cumulative_return(return_arr, axis=0):
+        return np.nancumprod(1 + return_arr, axis = axis)
+    
+    @staticmethod
+    def mean_return(return_arr, axis=0):
+        return np.nanmean(return_arr, axis = axis)
+    
+    @staticmethod
+    def volitility(return_arr, axis=0):
+        return np.nanstd(return_arr, axis = axis)
+    
+    @staticmethod
+    def annualized_return(return_arr, annualization_multiplier=252, axis=0):
+        power = annualization_multiplier/return_arr.shape[axis]
+        return np.power(1 + return_arr, power) - 1
+    
+    @staticmethod
+    def annualized_vol(return_arr, annnualization_multiplier=252, axis=0):
+        multiplier = np.sqrt(annnualization_multiplier)
+        return np.nanstd(return_arr, axis=axis)*multiplier
+    
+    @staticmethod
+    def 
+
+if __name__ == '__main__':
+    
+    
+    test_return_array = (np.random.rand(20,2) - 0.4)/10
+    test_return_array[:2,:] = np.nan
+    test_return_array[6,0] = np.nan
+    test_return_array[12,1] = np.nan
+    print(indicators.annualized_vol(test_return_array))
+    
+    
+    
+    
+    
+    
---
+++ ind.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  8 23:48:39 2018
+
+@author: x_Zero
+"""
+
+import pandas as pd
+import numpy as np
+pd.Series
+
+class rSeries(object):
+    
+    def __init__(self, series):
+        self.series = series
+        if not isinstance(series, pd.Series):
+            raise TypeError('Input is not pandas.Series type.')            
+    
+    @property
+    def cumulative_return(self):
+        return (self.series + 1).cumprod(skipna=True)
+    
+    @property
+    def accumulative_return(self):
+        return self.cumulative_return.iloc[-1]
+    
+    @property
+    def mean_return(self):
+        return self.series.mean(skipna=True)
+    
+    @property
+    def std(self):
+        return self.series.std()
+
+
+if __name__ == '__main__':
+    
+    
+    test_return_array = (np.random.rand(20) - 0.4)/10
+    test_return_array[:2] = np.nan
+    test_return_array[6] = np.nan
+    test_return_array[12] = np.nan
+    test_series = pd.Series(test_return_array)
+    tools = rSeries(test_series)
+    print(tools.cumulative_return)
+    
+    test_frame = pd.DataFrame((np.random.rand(20,2) - 0.4)/10)
+    print(test_frame.apply(lambda x:rSeries(x).mean_return, axis=0))
